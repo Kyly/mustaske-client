9c320f59ba23ffa9a9b5745379a9f669c831fc59
Chris Stringham <cstringham@satellite-air.dynamic.ucsd.edu>
2016-01-28 15:39:24 -0800

    Fixed that stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -206,7 +206,7 @@ module.exports = function (grunt) {
     // Compiles Sass to CSS and generates necessary files if requested
     compass: {
       options: {
-        sassDir: '<%= yeoman.app %>/styles/**/*',
+        sassDir: '<%= yeoman.app %>/styles',
         cssDir: '.tmp/styles',
         generatedImagesDir: '.tmp/images/generated',
         imagesDir: '<%= yeoman.app %>/images',
@@ -408,7 +408,7 @@ module.exports = function (grunt) {
       ],
       dist: [
         'compass:dist',
-        'imagemin',
+        //'imagemin',
         'svgmin'
       ]
     },
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,7 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch',
+    // 'ngTouch',
     'ngMaterial'
   ])
   .config(function ($routeProvider) {
