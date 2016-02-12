2111961b948d3c9258fa06293ed865f0d050ea62
Chris Stringham <C.J.Stringham@gmail.com>
2016-02-12 01:41:21 -0800

    post

--- post_install.sh
+++ post_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 npm install karma
 npm install phantomjs-prebuilt
-gem install compass
+npm install jasmine-core
+gem install compass --save-dev
 ./node_modules/bower/bin/bower install
 ./node_modules/grunt-cli/bin/grunt
