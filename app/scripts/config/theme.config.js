102501cf8ac2e18dd05d1c7382d18ce0e874fae9
Chris Stringham <C.J.Stringham@gmail.com>
2016-02-14 17:10:51 -0800

    Some theming fixes.

--- app/scripts/config/theme.config.js
+++ app/scripts/config/theme.config.js
@@ -24,42 +24,44 @@
   {
     themingProvider.definePalette(
       'mustaskePrimary', {
-        '50': '#bad9ee',
-        '100': '#7db8de',
-        '200': '#519fd3',
-        '300': '#2c7bb0',
-        '400': '#266a97',
-        '500': '#20597f',
-        '600': '#1a4867',
-        '700': '#14374e',
-        '800': '#0e2636',
-        '900': '#07141d',
-        'A100': '#bad9ee',
-        'A200': '#7db8de',
-        'A400': '#266a97',
-        'A700': '#14374e',
+        '50': '#3f7baf',
+        '100': '#386e9c',
+        '200': '#316189',
+        '300': '#2a5377',
+        '400': '#244664',
+        '500': '#1D3951',
+        '600': '#162c3e',
+        '700': '#101f2b',
+        '800': '#ffffff',
+        '900': '#ffffff',
+        'A100': '#4988be',
+        'A200': '#5c94c5',
+        'A400': '#6fa1cb',
+        'A700': '#ffffff',
         'contrastDefaultColor': 'light',
         'contrastDarkColors': '50 100 200 A100 A200'
       });
 
-    var mustaskeAccent = {
-      '50': '#ffffff',
-      '100': '#f9f9f9',
-      '200': '#ececec',
-      '300': '#dfdfdf',
-      '400': '#d3d3d3',
-      '500': '#C6C6C6',
-      '600': '#b9b9b9',
-      '700': '#acacac',
-      '800': '#a0a0a0',
-      '900': '#939393',
-      'A100': '#ffffff',
-      'A200': '#ffffff',
-      'A400': '#ffffff',
-      'A700': '#868686'
-    };
+    //var mustaskeAccent = {
+    themingProvider.definePalette(
+      'mustaskeAccent',{
+        '50': '#ffffff',
+        '100': '#ffffff',
+        '200': '#ffffff',
+        '300': '#fbfbfb',
+        '400': '#efefef',
+        '500': '#E2E2E2',
+        '600': '#d5d5d5',
+        '700': '#c8c8c8',
+        '800': '#bcbcbc',
+        '900': '#afafaf',
+        'A100': '#ffffff',
+        'A200': '#ffffff',
+        'A400': '#ffffff',
+        'A700': '#a2a2a2'
+    });
 
-    themingProvider.definePalette('mustaskeAccent', mustaskeAccent);
+    //themingProvider.definePalette('mustaskeAccent', mustaskeAccent);
 
     themingProvider.theme('default')
       .primaryPalette('mustaskePrimary')
