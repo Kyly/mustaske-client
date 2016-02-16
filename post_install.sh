97645c462ced17a5fc892fa39300cd22a21b0fb3
Kyly <kylyvass@gmail.com>
2016-02-16 14:59:52 -0800

    added dist back

--- post_install.sh
+++ post_install.sh
@@ -3,6 +3,5 @@ npm install karma
 npm install phantomjs-prebuilt
 npm install jasmine-core
 gem install compass
-npm install grunt grunt-cli
 ./node_modules/.bin/bower install
-./node_modules/.bin/grunt build
+#./node_modules/.bin/grunt build
