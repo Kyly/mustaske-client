7d746d6f0e33a9e085dc698dac6e354083989bf5
Kyly <kylyvass@gmail.com>
2016-02-16 14:45:20 -0800

    Edited heroku build script

--- post_install.sh
+++ post_install.sh
@@ -3,6 +3,6 @@ npm install karma
 npm install phantomjs-prebuilt
 npm install jasmine-core
 gem install compass
-npm install
-./node_modules/bower/bin/bower install
-./node_modules/grunt-cli/bin/grunt build
+npm install grunt grunt-cli
+./node_modules/.bin/bower install
+./node_modules/.bin/grunt build
