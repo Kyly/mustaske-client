8dd2e7bfc378ca54831f200966e6623fac373bb2
Chris Stringham <C.J.Stringham@gmail.com>
2016-02-16 14:31:39 -0800

    heroku stuff

--- post_install.sh
+++ post_install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-npm install karma
-npm install phantomjs-prebuilt
-npm install jasmine-core
-gem install compass
+#npm install karma
+#npm install phantomjs-prebuilt
+#npm install jasmine-core
+#gem install compass
 ./node_modules/bower/bin/bower install
-
-#./node_modules/grunt-cli/bin/grunt
+./node_modules/grunt-cli/bin/grunt
