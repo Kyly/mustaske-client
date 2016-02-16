4280609811cd54a274a6c6933e5040a58511aa19
Kyly <kylyvass@gmail.com>
2016-02-16 14:40:42 -0800

    Merge branch 'development' of github.com:Kyly/mustaske-client into development
    Conflicts:
    	dist/index.html
    	dist/views/poll.view.html

--- post_install.sh
+++ post_install.sh
@@ -3,5 +3,6 @@ npm install karma
 npm install phantomjs-prebuilt
 npm install jasmine-core
 gem install compass
+npm install
 ./node_modules/bower/bin/bower install
 ./node_modules/grunt-cli/bin/grunt build
