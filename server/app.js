fa64f946e0722bad95944298cc55fd45ea09d082
Kyly <kylyvass@gmail.com>
2016-03-01 16:10:45 -0800

    Edited npm start with production enviroment variable

--- server/app.js
+++ server/app.js
@@ -14,6 +14,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+console.log('ENV', app.get('env'));
 if (app.get('env') === 'development') {
   app.use('/tmp', gzippo.staticGzip(__dirname + '/../.tmp'));
     app.use(
